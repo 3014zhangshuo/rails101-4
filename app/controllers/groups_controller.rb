97aaa86db48881f20c00ac46214bdab1668a6e83
zhangshuo <3014zhangshuo@gmail.com>
2016-09-28 19:57:07 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,6 +29,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path, notice: "Upadate Success"
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
+
 
 end
 
