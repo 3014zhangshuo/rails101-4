9b30dd6aaead16286e0c78a8d744d464a86894da
zhangshuo <3014zhangshuo@gmail.com>
2016-09-29 08:53:39 +0800

    add

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,34 +1,31 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
-  def index
+before_action :authenticate_user! , only: [:new, :create]
+def index
     @groups = Group.all
   end
-
-  def new
-    @group = Group.new
+  def index
+    @groups = Group.new
   end
-
   def show
-    @group = Group.find(params[:id])
-  end
-
-  def edit
-    @group = Group.find(params[:id])
-  end
+       @group = Group.find(params[:id])
+ end
+ def edit
+  @group = Group.find(params[:id])
+ end
 
   def create
-    @group = Group.new(group_params)
+    @group = Group.create(group_params)
+
     if @group.save
-    redirect_to groups_path
+      redirect_to groups_path
     else
       render :new
     end
   end
-
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-    redirect_to groups_path, notice: "Upadate Success"
+      redirect_to groups_path, notice: "Update Success"
     else
       render :edit
     end
@@ -38,13 +35,13 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
-    redirect_to groups_path
+        redirect_to groups_path
   end
 
 
-end
+  private
 
-private
-def group_params
-  params.require(:group).permit(:title, :description)
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
